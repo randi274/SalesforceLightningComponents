811e4b83168dc8db70372c8bccf404a84951da52
randi274 <randi274@gmail.com>
2017-03-19 03:07:48 -0700

    Number Update

--- src/aura/CollectionComponent/CollectionComponentController.js
+++ src/aura/CollectionComponent/CollectionComponentController.js
@@ -1,5 +1,5 @@
 ({
-    getAccounts: function(cmp){
+    getOpportunities: function(cmp){
         var action = cmp.get("c.getOpps");
         action.setCallback(this, function(response){
             var state = response.getState();
@@ -7,6 +7,19 @@
                 var test = response.getReturnValue();   
                 //console.log(test);
                 cmp.set("v.OppList", test);
+            }
+        });
+        $A.enqueueAction(action);
+    },
+    getAmounts: function(cmp){
+        var action = cmp.get("c.getAmts");
+        action.setCallback(this, function(response){
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                var test = response.getReturnValue();   
+                console.log(test);
+                cmp.set("v.OppAmt", test[0]);
+                cmp.set("v.OppCom", test[1])
             }
         });
         $A.enqueueAction(action);
